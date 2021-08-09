4cf06c4841c2d671c0b2d08c72562f2f28040f1a
Manas Talukdar <manas.talukdar@gmail.com>
2021-08-08 18:41:21 -0700

    Cleanup.

--- scalacode/src/main/scala/Code.scala
+++ scalacode/src/main/scala/Code.scala
@@ -1,8 +1,9 @@
-object code 
-{
+object code  {
     // Main Method 
     def main(args: Array[String]) = {
         // prints Hello World
         println("Hello World!") 
     }
+
+    def msg = "I was compiled by Scala 3. :)"
 }
